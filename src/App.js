1943af575e63a602cb9e673fc0ae429c219ffcb6
ArvinDing <dingarvin@gmail.com>
2023-09-11 09:08:54 -0700

    Created skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Board from './Board.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Rectangle Example</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Board = () => {
+  const boardStyle = {
+    width: '200px',
+    height: '100px',
+    backgroundColor: 'blue',
+  };
+
+  return (
+    <div style={boardStyle}>
+      {/* Content goes here */}
+    </div>
+  );
+};
+
+export default Board;
